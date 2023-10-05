7c3172482909f985816a6920169f2d0b0b5e74c8
aitemrar-younes <aitemrar.younes@gmail.com>
2023-10-05 08:46:30 +0100

    adding select

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
+import Select from "react-select";
 /* import Modal from './components/Modal/Modal'; */
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Dialog } from "@mui/material";
 import Header from "./components/Header/Header";
 import Task from "./components/task/Task";
 
-
 function App() {
-  const endPoint = 'http://127.0.0.1:8000/api/todo/task/'
-  const [tasks, setTasks] = useState([])
-  const [task, setTask] = useState({"title":"", "description":""})
+  const endPoint = "http://127.0.0.1:8000/api/todo/";
+  const refColor = useRef();
+  const [updateData, setUpdateData] = useState(false)
+  const [colors, setColors] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState({ title: "", description: "", color:null });
   const submit_data = (event) => {
-    event.preventDefault()
-    fetch(endPoint,{
+    event.preventDefault();
+    fetch(endPoint+'task/',{
       method : 'POST',
       headers : {"Content-Type":"application/json"},
-      body : JSON.stringify({...task, color:1})
+      body : JSON.stringify(task)
     })
     .then(response=>{
       if (response.ok ){
@@ -29,9 +32,9 @@ function App() {
     .catch(err =>{ 
       console.log(err.message); 
     })
-  }
+  };
   const retrieve_data = () => {
-    fetch(endPoint)
+    fetch(endPoint + "task/")
       .then((response) => {
         if (response.ok) return response.json();
         else {
@@ -43,45 +46,85 @@ function App() {
       .then((data) => {
         setTasks(data);
       })
-      .catch((err) => {setTasks([]); console.log(err)});
-
+      .catch((err) => {
+        setTasks([]);
+        console.log(err);
+      });
   };
-  useEffect(()=>{
+  const retrieve_colors_data = () => {
+    fetch(endPoint + "color/")
+      .then((response) => {
+        if (response.ok) return response.json();
+        else {
+          throw Error(
+            "Could not get the response , status = " + response.status
+          );
+        }
+      })
+      .then((data) => {
+        setColors(data);
+      })
+      .catch((err) => {
+        setColors([]);
+        console.log(err);
+      });
+  };
+  useEffect(() => {
     retrieve_data();
-  },[])
+    retrieve_colors_data();
+  }, [updateData]);
 
   const [openDialogue, setOpenDialogue] = useState(false);
 
   return (
     <div className="App">
       <>
-      <Header setOpenDialogue={setOpenDialogue} />
+        <Header setOpenDialogue={setOpenDialogue} />
 
-      <div className="taskList">
-        { tasks.map( task =>(
-          <Task task={task} />
-        ))}
-      </div>
-
-      <Dialog open={openDialogue} onClose={() => setOpenDialogue(false)}>
-        <div className="dialogue">
-          <div className="dialogue_title">New Task</div>
-          <input type="text" className="input" placeholder="Title" value={task.title} onChange={(value)=>setTask({...task, "title":value.target.value})} />
-          <textarea
-            value={task.description}
-            onChange={(value)=>setTask({...task, "description":value.target.value})}
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            className="textArea"
-            placeholder="Description ..."
-            aria-setsize={false}
-          />
-          <button className="btn" onClick={submit_data}>Valide</button>
+        <div className="taskList">
+          {tasks.map((task) => (
+            <Task task={task} />
+          ))}
         </div>
-      </Dialog>
-      
+
+        <Dialog open={openDialogue} onClose={() => setOpenDialogue(false)}>
+          <div className="dialogue">
+            <div className="dialogue_title">New Task</div>
+            <input
+              type="text"
+              className="input"
+              placeholder="Title"
+              value={task.title}
+              onChange={(value) =>
+                setTask({ ...task, title: value.target.value })
+              }
+            />
+            <Select
+              ref={refColor}
+              onChange={choice => setTask({...task, color:choice.value})}
+              options={colors.map((color) => ({
+                value: color.id,
+                label: color.name,
+                obj:color 
+              }))}
+            />
+            <textarea
+              value={task.description}
+              onChange={(value) =>
+                setTask({ ...task, description: value.target.value })
+              }
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              className="textarea"
+              placeholder="Description ..."
+            />
+            <button className="btn" onClick={submit_data}>
+              Valide
+            </button>
+          </div>
+        </Dialog>
       </>
     </div>
   );
