7764f335fcd16eb718b35875cd791c713d414e5a
aitemrar-younes <aitemrar.younes@gmail.com>
2023-10-18 15:41:37 +0100

    tiny changes in dates todo

--- backend/root/todo/models.py
+++ backend/root/todo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
 class Color(models.Model):
     name = models.CharField(max_length=10, unique=True)
     code = models.CharField(max_length=10, unique=True)
@@ -15,18 +16,17 @@ class Color(models.Model):
         return super(Color, self).save(*args, **kwargs)
     
 class Task(models.Model):
+    """ right_now = timezone.now() """
     title = models.CharField(max_length=50)
     description = models.TextField()
     color = models.ForeignKey(Color, on_delete=models.PROTECT)
     archived = models.BooleanField(default=False)
-    created_at = models.DateTimeField(editable=False, default=timezone.now())
-    edited_at = models.DateTimeField(editable=False, default=timezone.now())
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.title)
-
+""" 
     def save(self, *args, **kwargs):
-        if not self.created_at:
-            self.created_at = timezone.now()
         self.edited_at = timezone.now()
-        return super(Task, self).save(*args, **kwargs)
+        return super(Task, self).save(*args, **kwargs) """
